1e4c28321562de8d40278e380829d48905e3b627
Alexander Nikolaychuk <alexksnikol@gmail.com>
2021-01-22 12:52:10 +0200

    Tested that delete delivers no error on non empty cache

--- Tests/FeedStoresTests/CoreDataFeedStoreTests/CoreDataFeedStoreChallengeTests.swift
+++ Tests/FeedStoresTests/CoreDataFeedStoreTests/CoreDataFeedStoreChallengeTests.swift
@@ -66,9 +66,9 @@ class CoreDataFeedStoreChallengeTests: XCTestCase, FeedStoreSpecs  {
 	}
 	
 	func test_delete_deliversNoErrorOnNonEmptyCache() {
-		//		let sut = makeSUT()
-		//
-		//		assertThatDeleteDeliversNoErrorOnNonEmptyCache(on: sut)
+		let sut = makeSUT()
+
+		assertThatDeleteDeliversNoErrorOnNonEmptyCache(on: sut)
 	}
 	
 	func test_delete_emptiesPreviouslyInsertedCache() {
